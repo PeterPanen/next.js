eb88766310d6a3d21a543a75119ddb71a8aa972e
Zack Tanner <1939140+ztanner@users.noreply.github.com>
2025-02-24 12:20:58 -0800

    [dev-overlay]: rename setAppIsrStatus to setIsrStatus

--- packages/next/src/server/lib/router-utils/setup-dev-bundler.ts
+++ packages/next/src/server/lib/router-utils/setup-dev-bundler.ts
@@ -108,7 +108,7 @@ export type ServerFields = {
   interceptionRoutes?: ReturnType<
     typeof import('./filesystem').buildCustomRoute
   >[]
-  setAppIsrStatus?: (key: string, value: boolean) => void
+  setIsrStatus?: (key: string, value: boolean) => void
   resetFetch?: () => void
 }
 
