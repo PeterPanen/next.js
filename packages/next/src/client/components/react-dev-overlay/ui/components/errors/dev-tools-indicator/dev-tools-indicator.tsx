3d79be142749706911a1487a0dc47323cafa2c31
devjiwonchoi <devjiwonchoi@gmail.com>
2025-02-24 22:27:10 +0900

    use devIndicator state

--- packages/next/src/client/components/react-dev-overlay/ui/components/errors/dev-tools-indicator/dev-tools-indicator.tsx
+++ packages/next/src/client/components/react-dev-overlay/ui/components/errors/dev-tools-indicator/dev-tools-indicator.tsx
@@ -50,7 +50,7 @@ export function DevToolsIndicator({
       setIsErrorOverlayOpen={setIsErrorOverlayOpen}
       isTurbopack={!!process.env.TURBOPACK}
       position={position}
-      disabled={state.disableDevIndicator || !isDevToolsIndicatorVisible}
+      disabled={state.devIndicator.isDisabled || !isDevToolsIndicatorVisible}
       isBuildError={isBuildError}
     />
   )
