09e2a67bb89f05e205cc9c91be55687b5eebd9a1
Zack Tanner <1939140+ztanner@users.noreply.github.com>
2025-02-24 16:32:22 -0800

    [dev-overlay]: wire up ISR status for pages router

--- packages/next/src/client/components/react-dev-overlay/pages/hooks.ts
+++ packages/next/src/client/components/react-dev-overlay/pages/hooks.ts
@@ -1,16 +1,24 @@
 import React from 'react'
 import * as Bus from './bus'
 import { useErrorOverlayReducer } from '../shared'
+import { Router } from '../../../router'
 
 export const usePagesDevOverlay = () => {
   const [state, dispatch] = useErrorOverlayReducer()
 
+  const { handleStaticIndicator } =
+    require('./hot-reloader-client') as typeof import('./hot-reloader-client')
+
   React.useEffect(() => {
     Bus.on(dispatch)
+
+    Router.events.on('routeChangeComplete', handleStaticIndicator)
+
     return function () {
+      Router.events.off('routeChangeComplete', handleStaticIndicator)
       Bus.off(dispatch)
     }
-  }, [dispatch])
+  }, [dispatch, handleStaticIndicator])
 
   const onComponentError = React.useCallback(
     (_error: Error, _componentStack: string | null) => {
